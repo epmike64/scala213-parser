a8a814a4997aaf972da13f2b12115291b276f4a5
epmike64 <epmike64@gmail.com>
2025-02-07 18:28:44 -0500

    Editing...

--- src/main/java/com/flint/compiler/tree/leaves/values/ClassDefLeafValue.java
+++ src/main/java/com/flint/compiler/tree/leaves/values/ClassDefLeafValue.java
@@ -1,5 +1,6 @@
 package com.flint.compiler.tree.leaves.values;
 
+import com.flint.compiler.token.fTokenKind;
 import com.flint.compiler.token.type.fToken;
 import com.flint.compiler.tree.leaves.nodes.ProdRootLeafN;
 
@@ -15,5 +16,6 @@ public class ClassDefLeafValue extends TokenLeafValue {
 
 	public ClassDefLeafValue(fToken kw_token) {
 		super(kw_token);
+		assert kw_token.kind == fTokenKind.T_CLASS;
 	}
 }
